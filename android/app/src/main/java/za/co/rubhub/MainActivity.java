d429b48374df97d5b2ee1b2b23ff49ee3ed44750
Chris Masina <thato.don@gmail.com>
2025-11-11 22:33:51 -0800

    testing config for Hithub aaactions

--- android/app/src/main/java/za/co/rubhub/MainActivity.java
+++ android/app/src/main/java/za/co/rubhub/MainActivity.java
@@ -2,4 +2,54 @@ package za.co.rubhub;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        
+        // WebView settings
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+
+        // Fix for cleartext traffic
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WebView.setWebContentsDebuggingEnabled(true);
+        }
+
+        // Handle SSL and cleartext
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
+
+        // Use your computer's IP address instead of localhost
+        String serverUrl = "http://192.168.1.100:3000"; // Replace with your computer's IP
+        
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                // Load fallback page on error
+                String errorHtml = "<html><body><h1>Error</h1><p>Failed to load: " + description + "</p></body></html>";
+                webView.loadData(errorHtml, "text/html", "UTF-8");
+            }
+        });
+
+        webView.loadUrl(serverUrl);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
