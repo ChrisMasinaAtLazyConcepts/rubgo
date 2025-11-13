8081fa3fc2596eace085ab7d919b5ff89a6c361d
Chris Masina <thato.don@gmail.com>
2025-11-12 19:59:50 -0800

    testing config for Hithub aaactions

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
-
 // app/layout.tsx
 import { AuthProvider } from '@/lib/auth-context'
+import './globals.css' // Make sure this import exists
+
 export const metadata = {
   title: 'RubHub - Massages on the go',
   description: 'Massage on demand system',
 };
-
 
 export default function RootLayout({
   children,
