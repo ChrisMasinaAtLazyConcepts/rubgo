ab202e7015f7b7991f493d2fa7f5176ed912a6d4
Chris Masina <thato.don@gmail.com>
2025-11-12 16:39:33 -0800

    testing config for Hithub aaactions

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'za.co.rubhub',
   appName: 'RubHub',
-  webDir: 'dist', // Use 'out' for Next.js build output
+  webDir: 'out', // Use 'out' for Next.js build output
   server: {
     url: 'http://192.168.0.81:3000', // Your computer's IP for development
     cleartext: true, // Allow HTTP traffic
